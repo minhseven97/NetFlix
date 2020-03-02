c8747e53ae119bdb7172dc826011e8102f0f217d
minh seven <minhvip245@outlook.com>
2020-03-02 11:06:07 +0700

    Add project files.

---
+++ Netflix/Netflix/MoviesPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Linq;
+
+namespace Netflix
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MoviesPage : ContentPage
+    {
+        private MovieService _service = new MovieService();
+
+        
+        private BindableProperty IsSearchingProperty =
+            BindableProperty.Create("IsSearching", typeof(bool), typeof(MoviesPage), false);
+        public bool IsSearching
+        {
+            get { return (bool)GetValue(IsSearchingProperty); }
+            set { SetValue(IsSearchingProperty, value); }
+        }
+        public MoviesPage()
+        {
+            BindingContext = this;
+            InitializeComponent();
+        }
+        async Task FindMovies(string actor)
+        {
+            
+            try
+            {
+                IsSearching = true;
+
+                var movies = await _service.FindMoviesByActor(actor);
+                
+                moviesListView.ItemsSource = movies;
+                moviesListView.IsVisible = movies.Any();
+                notFound.IsVisible = !moviesListView.IsVisible;
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "Could not retrieve the list of movies.", "OK");
+            }
+            finally
+            {
+                IsSearching = false;
+            }
+        }
+        async private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == null || e.NewTextValue.Length < MovieService.MinSearchLength)
+                return;
+
+       
+            await FindMovies(actor: e.NewTextValue);
+        }
+
+        async private void OnMovieSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var movie = e.SelectedItem as Movie;
+
+            moviesListView.SelectedItem = null;
+
+            await Navigation.PushAsync(new MovieDetailsPage(movie));
+        }
+    }
+}
